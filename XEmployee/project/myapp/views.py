35a5324719a08089b866e04145e9bff90f3926d5
yashaswini-mn <yashaswnimn26@gmail.com>
2025-02-24 06:00:39 +0530

    final commit

--- XEmployee/project/myapp/views.py
+++ XEmployee/project/myapp/views.py
@@ -7,19 +7,21 @@ from django.contrib import messages
 def employee_list(request):
     if request.method=='POST':
         empname = request.POST.get('empname')
-        empmail=request.POST.get('empmail')
+        empemail=request.POST.get('empeemail')
         pno = request.POST.get("pno")
-        department=request.POST("department")
-        designation=request.POST("designation")
-        salary=request.POST("salary")
-        date=request.POST("date")
-        empType=request.POST("empType")
-        address=request.POST("address")
-        status=request.POST("status")
+        department=request.POST.get("department")
+        designation=request.POST.get("designation")
+        
+        salary=request.POST.get("salary")
+        date=request.POST.get("date")
+        empType=request.POST.get("empType")
+        address=request.POST.get("address")
+        status=request.POST.get("status")
 
-        employee=Employees(empname=empname,empmail=empmail,pno=pno,department=department,designation=designation,salary=salary,date=date,empType=empType,address=address, status=status )
+        employee=Employees(empname=empname,empemail=empemail,pno=pno,department=department,designation=designation,salary=salary,date=date,empType=empType,address=address, status=status )
 
         employee.save()
+        messages.success(request, "Employee added successfully")
         return redirect('employee_list')
     employees= Employees.objects.all()
     return render(request, 'index.html', {'employees':employees})
@@ -28,7 +30,7 @@ def update_employee(request, pk):
     employee = get_object_or_404(Employees, pk=pk)
     if request.method=='POST':
         empname = request.POST.get('empname')
-        empmail=request.POST.get('empmail')
+        empemail=request.POST.get('empemail')
         pno = request.POST.get("pno")
         department=request.POST.get("department")
         designation=request.POST.get("designation")
@@ -39,7 +41,7 @@ def update_employee(request, pk):
         status=request.POST.get("status")
 
         employee.empname =empname
-        employee.empmail=empmail
+        employee.empemail=empemail
         employee.pno =pno
         employee.department=department
         employee.designation=designation
@@ -50,12 +52,12 @@ def update_employee(request, pk):
         employee.status=status
     
         employee.save()
-        messages.success(request,"Updated")
+        messages.success(request,"Employee Updated Successfully")
         return redirect('employee_list')
     return render(request, 'update_employee.html',{'employee':employee} )
 
 def delete_student(request, pk):
-    student = get_object_or_404(Students, pk=pk)
-    student.delete()
-    messages.success(request,"Deleted")
-    return redirect('student_list')
+    employee = get_object_or_404(Employees, pk=pk)
+    employee.delete()
+    messages.success(request,"Employee Deleted Successfully")
+    return redirect('employee_list')
